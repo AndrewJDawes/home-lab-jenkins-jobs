83c7ea2c0edceb07f947e8633b6ed1270a01df05
Andrew Dawes <andrewjamesdawes@gmail.com>
2025-09-27 15:05:31 -0400

    Correct

--- jobs/github/organizations/codekaizen-github/deploy/job.groovy
+++ jobs/github/organizations/codekaizen-github/deploy/job.groovy
@@ -7,9 +7,11 @@ organizationFolder('jobs-v2/github/organizations/codekaizen-github/deploy') {
 
     displayName("CodeKaizen GitHub Deploy")
 
-    suppressFolderAutomaticTriggering {
-        branches("*")
-        strategy("INDEXING")
+    properties {
+        suppressFolderAutomaticTriggering {
+            branches("*")
+            strategy("INDEXING")
+        }
     }
 
  // Requires this plugin: https://plugins.jenkins.io/basic-branch-build-strategies/
